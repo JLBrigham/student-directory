1302b501b893b2aa2273d10eb8c0f2a8aade9524
Joanna Brigham <joannabrigham@hotmail.co.uk>
2021-01-28 12:18:16 +0100

    Refactor interactive_menu method

--- directory.rb
+++ directory.rb
@@ -1,31 +1,8 @@
-def interactive_menu
-  students = []
-  loop do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-    selection = gets.chomp
-    case selection
-    when "1"
-      students = input_students
-    when "2"
-      print_header
-      print(students)
-      print_footer(students)
-    when "9"
-      exit #this will cause the program to terminate
-    else
-      puts "I don't know what you meant, try again"
-  end
-  end
-end
-
+@students = []
 
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  # create an empty array
-  students = []
   # get the first name
   name = gets.chomp
   # while the name is not empty, repeat this code
@@ -36,63 +13,92 @@ def input_students
     cohort = "default"
   end
   #add the student hash to the array
-  students << {name: name, cohort: cohort}
+  @students << {name: name, cohort: cohort}
 
-  if students.count == 1
-  puts "Now we have #{students.count} student. Give me the next name"
+  if @students.count == 1
+  puts "Now we have #{@students.count} student. Give me the next name"
   else
-    puts "Now we have #{students.count} students. Give me the next name"
+    puts "Now we have #{@students.count} students. Give me the next name"
   # get another name from the user
   end
   # get another name from the user
   name = gets.chomp
   end
   # return the array of input_students
-  students
 end
+
+
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_student_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "9"
+    exit #this will cause the program to terminate
+  else
+    puts "I don't know what you meant, try again"
+end
+end
+
 
 def print_header
 puts "The students of Villains Academy".center(80)
 puts "-------------".center(80)
 end
 
-def print(students)
-  students.each.with_index(1) do |student, index|
+def print_student_list
+  @students.each.with_index(1) do |student, index|
     puts "#{index}. #{student[:name]} (#{student[:cohort]} cohort)".center(80)
   end
 end
 
-def print_footer(students)
-puts "Overall, we have #{students.count} great students".center(80)
+def print_footer
+puts "Overall, we have #{@students.count} great students".center(80)
 end
 
-def sorted_by_cohort(students)
- sorted_by_cohort = {}
+#def sorted_by_cohort(students)
+ #sorted_by_cohort = {}
 
- students.each do |student|
-   cohort = student[:cohort]
-   name = student[:name]
+ #students.each do |student|
+   #cohort = student[:cohort]
+   #name = student[:name]
 
-   if sorted_by_cohort[cohort] == nil
-     sorted_by_cohort[cohort] = [name]
-     else
-     sorted_by_cohort[cohort].push(name)
-   end
+   #if sorted_by_cohort[cohort] == nil
+     #sorted_by_cohort[cohort] = [name]
+     #else
+     #sorted_by_cohort[cohort].push(name)
+   #end
 
 
-  end
+  #end
 
-   sorted_by_cohort.each do|cohort, name|
-    puts cohort.upcase
-    puts name
-  end
+   #sorted_by_cohort.each do|cohort, name|
+    #puts cohort.upcase
+    #puts name
+  #end
 
-end
+#end
 
 
 #nothing happens until we call the methods
 interactive_menu
-print_header
-print(students)
-print_footer(students)
-sorted_by_cohort(students)
