bd679fdd08757ab33e3fa8d2b79df06e382ebd9b
Joanna Brigham <joannabrigham@hotmail.co.uk>
2021-01-28 10:12:03 +0100

    Sort students by cohort

--- directory.rb
+++ directory.rb
@@ -21,16 +21,42 @@ def print_header
 puts "The students of Villains Academy".center(80)
 puts "-------------".center(80)
 end
+
 def print(students)
   students.each.with_index(1) do |student, index|
     puts "#{index}. #{student[:name]} (#{student[:cohort]} cohort)".center(80)
   end
 end
+
 def print_footer(students)
 puts "Overall, we have #{students.count} great students".center(80)
+end
+
+def sorted_by_cohort(students)
+ sorted_by_cohort = {}
+
+ students.each do |student|
+   cohort = student[:cohort]
+   name = student[:name]
+
+   if sorted_by_cohort[cohort] == nil
+     sorted_by_cohort[cohort] = [name]
+     else
+     sorted_by_cohort[cohort].push(name)
+   end
+
+
+  end
+
+   sorted_by_cohort.each do|cohort, name|
+    puts cohort.upcase
+    puts name
+  end
+
 end
 #nothing happens until we call the methods
 students = input_students
 print_header
 print(students)
 print_footer(students)
+sorted_by_cohort(students)
